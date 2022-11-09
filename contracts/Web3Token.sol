59831c6f80789f26dad524c823c1fef8ba6e6ae6
0xWeb3Boy <113019033+0xWeb3boy@users.noreply.github.com>
2022-11-09 08:41:35 -0800

    Update Web3Token.sol

--- contracts/Web3Token.sol
+++ contracts/Web3Token.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.7;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Web3Token is ERC20 {
+    address public admin;
+
     constructor(uint256 initialSupply) ERC20("Web3Token", "WEB3") {
         _mint(msg.sender, initialSupply);
+        admin = msg.sender;
+    }
+
+    function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+    function mint(uint256 amount) public virtual {
+        require(msg.sender == admin, "Only admin");
+        _mint(_msgSender(), amount);
     }
 }
